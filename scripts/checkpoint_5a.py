58413c8d81b30099498807721c8a1deab73dfd68
Ben Wynne <bwynne@cern.ch>
2021-08-23 16:05:20 +0100

    Fixed DS18B20 name

--- scripts/checkpoint_5a.py
+++ scripts/checkpoint_5a.py
@@ -1,6 +1,6 @@
 # Import
-from DAH import DS18S20
+from DAH import DS18B20
 
 # Readout temperature sensor
-tmp0 = DS18S20(address="10-00080265b6d6")
+tmp0 = DS18B20(address="10-00080265b6d6")
 tmp0.getCelsius()
